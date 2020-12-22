0f719fde817a8b2f960a731117555f8d7c3f225b
yvhime <75004395+yvhime@users.noreply.github.com>
2020-12-22 16:26:14 +0800

    User files/pages
    Initially uploaded 8 files for user
    changepassword
    connection
    createnewaccount
    grades
    index
    login
    logout
    my_profile
    
    bootstrap design will not work
    php codes

---
+++ createnewaccount.php
@@ -0,0 +1,322 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+session_start();
+include("connection.php");
+
+$firstname = "";
+$lastname = "";
+$phonenumber = "";
+$email_address = "";
+$gender = "Male";
+$birthday = "";
+$address = "";
+
+if(isset($_POST['createaccount'])) {
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    //$username = $_POST['username'];
+    $email_address = $_POST['email_address'];
+    $pass_word = $_POST['pass_word'];
+    $phonenumber = $_POST['phonenumber'];
+    $gender = $_POST['gender'];
+    $birthday = $_POST['birthday'];
+    $address = $_POST['address'];
+    $passwordRaw = $_POST['pass_word']; //trial
+    $passwordHash = password_hash($passwordRaw, PASSWORD_DEFAULT); //trial
+    $repeatPassword = $_POST['repeatPassword'];
+    // echo "pw: " . $passwordRaw . "<br>";
+    // echo "repeat pw: " . $repeatPassword . "<br>";
+    //echo $passwordHash;
+    // to check if it works
+    // echo $firstname . "<br>";
+    // echo $lastname . "<br>";
+    // echo $username . "<br>";
+    // echo $email_address . "<br>";
+    // echo $pass_word . "<br>";
+    // echo $phonenumber . "<br>";
+    // echo $gender . "<br>";
+    // echo $birthday . "<br>";
+    // echo $address . "<br>";
+
+    // if ($passwordRaw == $repeatPassword) {
+    //     echo "<br>password match";
+    // } else {
+    //     echo "<br>password not matched";
+    // }
+
+    //to check if passwords match
+    if ($passwordRaw == $repeatPassword) {
+        //check whether there is a duplicate username
+        $check_duplicate = mysqli_query($connect, "SELECT id FROM users WHERE email_address = '$email_address' ");
+        $row = mysqli_num_rows($check_duplicate);
+
+        if($row > 0) {
+            echo "<script>alert('Username already exists. Choose a new one.')</script>";    
+        } 
+        //insert acc info to database redirect to index.php
+        //$passwordHash trial
+        else {
+            $input_account = mysqli_query($connect, "INSERT INTO users (firstname, lastname, email_address, password, phonenumber, gender, birthday, address) VALUES ('$firstname', '$lastname', '$email_address', '$passwordHash', '$phonenumber', '$gender', '$birthday', '$address') ");
+            echo "<script>alert('Create new account success.')</script>";
+            echo "<script> window.location='login.php' </script>";
+        }
+    } else {
+        echo "<script> alert('Passwords does not match.') </script>";
+    }
+
+}
+?>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>LOREM IPSUM</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<!-- <body class="bg-gradient-primary"> -->
+<body class="">
+    <!-- <div class="row justify-content-center"> -->
+    <!-- <div class="container">
+        <div class="col-xl-6 col-lg-6 col-md-9">
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0"> -->
+                <!-- Nested Row within Card Body -->
+                <!-- <div class="row"> -->
+                    <!-- <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>PUG IMAGE -->
+                    <!-- <div class="col-lg-12">
+                        <div class="p-5"> -->
+                            <!-- <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Create New Account</h1>
+                            </div> -->
+                            <!-- <form class="user"> -->
+                            <!-- <form method="POST" action="">
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0 form-outline"> -->
+                                        <!-- firstname -->
+                                        <!-- <input type="text" class="form-control form-control-user" name="firstname" id="firstname" placeholder="First Name" value="<?php echo $firstname ?>">
+                                    </div>
+                                    <div class="col-sm-6"> -->
+                                        <!-- lastname -->
+                                        <!-- <input type="text" class="form-control form-control-user" name="lastname" id="lastname" placeholder="Last Name" value="<?php echo $lastname ?>">
+                                    </div>
+                                </div> -->
+
+                                <!-- <div class="form-group row"> -->
+                                    <!-- <div class="col-sm-6 mb-3 mb-sm-0"> -->
+                                        <!-- username -->
+                                        <!-- <input type="text" class="form-control form-control-user" name="username" id="username" placeholder="Username"> -->
+                                    <!-- </div> -->
+                                    
+                                    <!-- <div class="col-sm-6"> -->
+                                        <!-- phonenumber -->
+                                        <!-- <input type="text" class="form-control form-control-user" name="phonenumber" id="phonenumber" placeholder="Phone number" value="<?php echo $phonenumber ?>">
+                                    </div>
+                                </div>
+
+                                <div class="form-group"> -->
+                                    <!-- email add -->
+                                    <!-- <input type="email" class="form-control form-control-user" name="email_address" id="email_address" placeholder="Email Address" value="<?php echo $email_address ?>">
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0"> -->
+                                        <!-- password -->
+                                        <!-- <input type="password" class="form-control form-control-user" name="pass_word" id="pass_word" placeholder="Password">
+                                    </div>
+                                    <div class="col-sm-6"> -->
+                                        <!-- repeat password -->
+                                        <!-- <input type="password" class="form-control form-control-user" name="repeatPassword" id="repeatPassword" placeholder="Repeat Password">
+                                    </div>                           
+                                </div>
+
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0"> -->
+                                        <!-- gender -->
+                                        <!-- <input type="text" class="form-control form-control-user" name="gender" id="gender" placeholder="Gender"> -->
+                                        <!-- <select name="gender" id="gender" class="form-control form-control-user">
+                                            <option disabled="">Gender</option>
+                                            <option <?php if($gender == "Male") echo "selected"; ?> value="Male">
+                                                Male</option>
+                                            <option <?php if($gender == "Female") echo "selected"; ?> value="Female">
+                                                Female</option>
+                                        </select>
+                                    </div>
+                                    <div class="col-sm-6"> -->
+                                        <!-- birthday -->
+                                        <!-- <input type="date" class="form-control form-control-user" name="birthday" 
+                                            id="birthday" placeholder="Birthday" value="<?php echo $birthday ?>">
+                                    </div>                           
+                                </div>
+
+                                <div class="form-group"> -->
+                                    <!-- address -->
+                                    <!-- <input type="text" class="form-control form-control-user" name="address" id="address" placeholder="Address" value="<?php echo $address ?>">
+                                </div> -->
+
+                                <!-- create acc button -->
+                                <!-- <input type="submit" class="btn btn-primary btn-user btn-block" name="createaccount" id="createaccount" value="Create Account">
+                            </form>
+                            <hr>
+                            <div class="text-center">
+                                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                            </div>
+                            <div class="text-center">
+                                <a class="small" href="login.php">Already have an account? Login!</a>
+                            </div> -->
+                        <!-- </div>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <!--</div>--> <!-- HERE -->
+    <!-- </div> 94 186 -->
+    <!-- </div> HERE AGAIN -->
+
+    <!-- --------------------------------------------- -->
+
+        <div class="row justify-content-center">
+
+            <div class="col-xl-6 col-lg-6 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> PUG IMAGE -->
+                            <div class="col-lg-12">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Create New Account</h1>
+                                    </div>
+                                    <!-- <form class="user"> -->
+                                    <form method="POST" action="">
+                                        <div class="form-group row">
+                                            <div class="col-sm-6 mb-3 mb-sm-0 form-outline">
+                                                <!-- firstname -->
+                                                <input type="text" class="form-control form-control-user" name="firstname" id="firstname" placeholder="First Name" value="<?php echo $firstname ?>">
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <!-- lastname -->
+                                                <input type="text" class="form-control form-control-user" name="lastname" id="lastname" placeholder="Last Name" value="<?php echo $lastname ?>">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <!-- <div class="col-sm-6 mb-3 mb-sm-0"> -->
+                                                <!-- username -->
+                                                <!-- <input type="text" class="form-control form-control-user" name="username" id="username" placeholder="Username"> -->
+                                            <!-- </div> -->
+                                            
+                                            <div class="col-sm-6">
+                                                <!-- phonenumber -->
+                                                <input type="text" class="form-control form-control-user" name="phonenumber" id="phonenumber" placeholder="Phone number" value="<?php echo $phonenumber ?>">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <!-- email add -->
+                                            <input type="email" class="form-control form-control-user" name="email_address" id="email_address" placeholder="Email Address" value="<?php echo $email_address ?>">
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <div class="col-sm-6 mb-3 mb-sm-0">
+                                                <!-- password -->
+                                                <input type="password" class="form-control form-control-user" name="pass_word" id="pass_word" placeholder="Password">
+                                            </div>
+                                            <div class="col-sm-6">
+                                                <!-- repeat password -->
+                                                <input type="password" class="form-control form-control-user" name="repeatPassword" id="repeatPassword" placeholder="Repeat Password">
+                                            </div>                           
+                                        </div>
+
+                                        <div class="form-group row">
+                                            <div class="col-sm-6 mb-3 mb-sm-0">
+                                                <!-- gender -->
+                                                <!-- <input type="text" class="form-control form-control-user" name="gender" id="gender" placeholder="Gender"> -->
+                                                <select name="gender" id="gender" class="form-control form-control-user">
+                                                    <option disabled="">Gender</option>
+                                                    <option <?php if($gender == "Male") echo "selected"; ?> value="Male">
+                                                        Male</option>
+                                                    <option <?php if($gender == "Female") echo "selected"; ?> value="Female">
+                                                        Female</option>
+                                                </select>
+                                            </div>
+
+                                            <div class="col-sm-6">
+                                                <!-- birthday -->
+                                                <input type="date" class="form-control form-control-user" name="birthday" 
+                                                    id="birthday" placeholder="Birthday" value="<?php echo $birthday ?>">
+                                            </div>                           
+                                        </div>
+
+                                        <div class="form-group">
+                                            <!-- address -->
+                                            <input type="text" class="form-control form-control-user" name="address" id="address" 
+                                                placeholder="Address" value="<?php echo $address ?>">
+                                        </div>
+
+                                        <!-- create acc button -->
+                                        <input type="submit" class="btn btn-primary btn-user btn-block" name="createaccount" id="createaccount" 
+                                            value="Create Account">
+                                        
+                                    </form>
+                                    <!-- </form> -->
+
+                                    <!-- <hr>
+
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    </div>
+
+                                    <div class="text-center">
+                                        <a class="small" href="createnewaccount.php">Create New Account</a>
+                                    </div> -->
+
+                                    <hr>
+
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    </div>
+
+                                    <div class="text-center">
+                                        <a class="small" href="login.php">Already have an account? Login!</a>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    <!-- --------------------------------------------- -->
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+session_start();
+include("connection.php");
+
+$email_address = ""; //putting a non value
+
+//can use _POST or _REQUEST / *_SESSION DOESNT WORK FOR SOME REASONS ON ISSET LOGIN 
+//to pass values by session
+if(isset($_REQUEST['login'])) {
+
+    $email_address = $_POST['email_address'];
+    $sql_user = mysqli_query($connect, "SELECT * FROM users WHERE email_address = '" . $email_address . "' ");
+    $sql_row = mysqli_fetch_array($sql_user);
+
+    if($sql_row > 0) {
+        $_SESSION['id'] = $sql_row['id'];
+        $_SESSION['firstname'] = $sql_row['firstname'];
+        $_SESSION['lastname'] = $sql_row['lastname'];
+        //$_SESSION['username'] = $sql_row['username'];
+        $_SESSION['email_address'] = $sql_row['email_address'];
+        $_SESSION['password'] = $sql_row['password'];
+        $_SESSION['phonenumber'] = $sql_row['phonenumber'];
+        $_SESSION['gender'] = $sql_row['gender'];
+        $_SESSION['birthday'] = $sql_row['birthday'];
+        $_SESSION['address'] = $sql_row['address'];
+        $_SESSION['program'] = $sql_row['program'];
+        $_SESSION['yearlevel'] = $sql_row['yearlevel'];
+        $_SESSION['semester'] = $sql_row['semester'];
+        //echo $_SESSION['yearlevel'];
+        //echo "<script>window.location = 'index.php'</script>";
+    }
+}
+
+//code for logging in
+//check whether the variable is empty
+//or is set/declared
+if(isset($_POST['login'])) { // email_address
+
+    // $email_address = $_POST['email_address'];
+    // $pass_word = $_POST['pass_word'];
+    // $passwordHash = password_hash($pass_word, PASSWORD_DEFAULT);
+    // $result = "SELECT * FROM users WHERE email_address = '".$email_address."' AND password = '".$pass_word."' ";
+    // //$connect came from include("connection.php");
+    // //always put mysqli/connect first
+    // $sql_info = mysqli_query($connect, $result);
+    // //i think == 1 and > 0 is the same
+    // //i think using mysqlinumrows checks if there is a
+    // //value stored in db not sure
+    // if(mysqli_num_rows($sql_info) > 0) {
+    //     //echo "<script>alert('Success!')</script>";
+    //     echo "<script>window.location = 'index.php'</script>";
+    // }
+    // else {
+    //     echo "<script>alert('Fail!')</script>";
+    // }
+
+    $emailAddress = mysqli_real_escape_string($connect, $_POST["email_address"]);  
+    $password = mysqli_real_escape_string($connect, $_POST["pass_word"]);  
+    $query = "SELECT * FROM users WHERE email_address = '".$emailAddress."'";  
+    $result = mysqli_query($connect, $query); 
+
+    if(mysqli_num_rows($result) > 0) {  
+        while($row = mysqli_fetch_array($result)) {  
+            if(password_verify($password, $row["password"])) {  
+                //return true;  
+                $_SESSION["email_address"] = $emailAddress;  
+                header("location:index.php");  
+            } else {  
+                //return false;  
+                echo '<script>alert("Wrong password")</script>';  
+                }  
+        }  
+    } else {  
+        echo '<script>alert("Wrong User Details")</script>';  
+        }
+
+}
+?>
+
+<!-- showcodes eye style trial -->
+<style type="text/css">
+    .passwordIcon {
+        float: right;
+        margin-left: -25px;
+        margin-top: -25px;
+        position: relative;
+        z-index: 2;
+    }
+</style>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>wkwkw</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="">
+
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-6 col-lg-6 col-md-9"> <!--10-->
+
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row"> <!--original / class="row"-->
+                            <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> PUG IMAGE-->
+                            <div class="col-lg-12"> <!--original / class="col-lg-6"-->
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back Student!</h1>
+                                    </div>
+                                    <!-- <form class="user"> -->
+                                        <form method="POST" action="login.php">
+                                        <div class="form-group">  <!--class="form-group"-->
+                                            <!-- enter emailaddress -->
+                                            <input type="text" class="form-control form-control-user" name="email_address" 
+                                                id="email_address" aria-describedby="emailHelp" value="<?php echo $email_address ?>" placeholder="Email Address">
+                                        </div>
+                                        <div class="form-group">
+                                            <!-- col-sm-6 mb-3 mb-sm-0 trystyle-->
+                                            <!-- form-group col-lg-11 row justify-content-left default -->
+
+                                            <!-- enter password -->
+                                            <!-- <div class="col-sm-11 mb-3 mb-sm-0">
+                                                <input type="password" class="form-control form-control-user" name="pass_word" id="pass_word" placeholder="Password">
+                                            </div> -->
+                                            <!-- <div class="col-sm-1">
+                                                <input type="checkbox" class="form-check-input" name="" id="showPass" onclick="showPassword()">
+                                                <label class="form-check-label" for="showPass"></label> -->
+
+                                                <!-- <span toggle="#password-field" class="fa fa-fw fa-eye passwordIcon toggle-password"></span> -->
+                                            <!-- </div> -->
+
+                                            <!-- this will work but have to edit the upper part  -->
+                                            <div class="input-group mb-3">
+                                                <input type="password" class="form-control" aria-label="Text input with checkbox"
+                                                placeholder="Password" name="pass_word" id="pass_word">
+                                                    <div class="input-group-prepend">
+                                                        <div class="input-group-text">
+                                                            <input type="checkbox" aria-label="Checkbox for following text input"
+                                                            id="showPass" onclick="showPassword()">
+                                                        </div>
+                                                    </div>
+                                            </div>
+
+                                        </div>
+                                        
+                                        <!-- login button -->
+                                        <input type="submit" name="login" id="login" value="Login" class="btn btn-primary btn-user btn-block">
+                                        </form>
+
+                                    <!-- </form> -->
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="createnewaccount.php">Create New Account</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- show password codes -->
+    <script> 
+        function showPassword() {
+            var passWord = document.getElementById("pass_word");
+            
+            if (passWord.type === "password") {
+                passWord.type = "text";
+            } else {
+                passWord.type = "password";
+            }
+        }
+    </script>
+
+</body>
+
+</html>
